0a4b3e5383f9e000a99d09ef64abc1393e8b022a
IulianStave <iulian.staverescu@gmail.com>
2019-10-29 15:23:06 +0200

    Add delete and update methods

--- sqlite_db.py
+++ sqlite_db.py
@@ -22,7 +22,24 @@ def view_data():
     rows = cursor.fetchall()
     conn.close()
     return rows
-insert_data('Water Glass', 42, 6.5)
-insert_data('Coffe cup', 2, 3.5)
+def delete_row(item):
+    conn = sqlite3.connect("lite.db")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM store WHERE item = ?",(item,))
+    conn.commit()
+    conn.close()
+def update(quantity, price, item):
+    conn = sqlite3.connect("lite.db")
+    cursor = conn.cursor()
+    cursor.execute("UPDATE store SET quantity =?, price=? WHERE item = ?",(quantity, price, item))
+    conn.commit()
+    conn.close()
+#insert_data('Water Glass', 42, 6.5)
+#insert_data('Coffe cup', 2, 3.5)
 print(view_data())
+delete_row('wine glasss')
+update(20,22,'aa')
+delete_row('Coffe cup')
+update(20,22,'Water Glass')
 
+print(view_data())
